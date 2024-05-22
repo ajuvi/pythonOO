c935f8dcb5e910bc0b1c46edf7a9c75814ca3d06
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-05-22 17:16:17 +0200

    artur

--- Futbol.py
+++ Futbol.py
@@ -1,15 +1,21 @@
 class Persona:
+
     def __init__(self,nom,cognoms):
         self.nom=nom
         self.cognoms=cognoms
 
     def mostrar(self):
-        print(f"Persona: {self.nom} {self.cognoms}")
+        print(self)
+
+    def __str__(self):
+        return f"Persona: {self.nom} {self.cognoms}"        
 
 class Futbolista(Persona):
+
     def __init__(self,nom,cognoms,equip):
         super().__init__(nom,cognoms)
         self.equip=equip
+        self.lesionat=False
 
     def mostrar(self):
         print(f"Futbolista: {self.nom} {self.cognoms}")
@@ -18,8 +24,10 @@ class Entrenador(Persona):
     def __init__(self,nom,cognoms,equip):
         super().__init__(nom,cognoms)
         self.equip=equip
+
     def mostrar_entrenador(self):
         print(self)
+
     def __str__(self):
         return f"Entrenador {self.equip}: {self.nom} {self.cognoms}"
 
